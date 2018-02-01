d6fc68ce9bb6b50a5f1eb9793d0326fd1e1b29dc
mvieiraah <matheus0322@gmail.com>
2018-02-01 00:15:21 -0200

    Update projetobspython.py

--- projetobspython.py
+++ projetobspython.py
@@ -1,22 +1,14 @@
-import requests
-from bs4 import BeautifulSoup
+#coding:utf-8
+try:
+  from bs4 import BeautifulSoup
+  import requests 
+except:
+  print ("Módulos não carregados")
 
-url= 'http://www.rj.gov.br/web/seeduc' 
+url = "http://www.rj.gov.br/web/seeduc/exibeconteudo?article-id=375402"
+r = requests.get (url)
+pega = r.text
+soup = BeautifulSoup(pega)  
 
-r = requests.get(url)
-
-soup = BeautifulSoup.BeautifulSoup(r.text,'html.parser')
- 
-lista_diretorias = soup.find_all('table': class='Diretorias_regionais'):
-#print(lista_diretorias)
-       
-url= 'http://www.rj.gov.br/web/seeduc/exibeconteudo?article-id=375402'
-
-for lista_td in lista_diretorias:
-    lista_td.findall('td')
-    for lista_dados in lista:
-    #print(lista_dados.next_element)
-        if lista_dados.next_element.name == 'a':
-           url_it = '{0}{1}'.format(url,lista_diretorias.next_element.get('href'))
-        else:
-            print(lista_dados.next_element)
+for links in soup.find_all('a'):
+  print (links.get('href'))
